1d27bf008954a1648f3597deeca353cc9f16aaa7
Daniel Palacio <dpalacio@buffalo.edu>
2019-12-12 12:26:07 -0500

    Initial Commit

---
+++ ChatMe/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ChatMe
+//
+//  Created by Daniel Palacio on 12/12/19.
+//  Copyright © 2019 Daniel Palacio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
